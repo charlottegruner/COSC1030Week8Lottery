74aad4f93a81e33d3fe4e8816ad5279cbd7af0bd
Charlotte Gruner <60362002+charlottegruner@users.noreply.github.com>
2025-03-10 11:47:29 -0600

    Add files via upload

---
+++ lottery.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <random>
+using namespace std; 
+
+int main() 
+{ 
+  // Write the code necessary to create an array to store 4 lottery numbers
+  // Generate the random lottery numbers from 1 to 30
+
+  // Write the code to create an array of user lottery numbers.  Have the user enter 
+  // the numbers
+
+  // Write the code to compare the two arrays to see how many match between the actual lottery
+  // numbers and the user numbers 
+
+}
